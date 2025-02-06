18948e03db1a2dbed7d5881f99a5423f5a50bf89
henry-hb <hall-brh@wlhs.wlwv.k12.or.us>
2025-02-06 09:10:18 -0800

    final commit

--- Lesson-2/rps_minus_one.py
+++ Lesson-2/rps_minus_one.py
@@ -40,25 +40,44 @@ def comp_keep(comp_hand):
     comp_keep_choice = random.randint(0,1)
     return comp_hand[comp_keep_choice]
 
-def winner_evaluation(comp_final, player_final):
-    
+def winner_evaluation(comp_final, player_final, comp_score, player_score):
+    if(comp_final == player_final):
+        return "TIE"
+    elif(player_final == "rock" and comp_final == "paper") or (player_final == "paper" and comp_final == "scissors") or (player_final == "scissors" and comp_final == "rock"):
+        return "LOSE"
+    else:
+        return "WIN"
 
 def main():
     # setup
-    comp_hand = ["",""]
-    player_hand = ["",""]
     comp_score = 0
     player_score = 0
+    keep_playing = True
+    while(keep_playing):
+        comp_hand = ["",""]
+        player_hand = ["",""]
+        
 
-    #generating hands
-    comp_hand = gen_comp_hand()
-    gen_player_hand(player_hand)
+        # generating hands
+        comp_hand = gen_comp_hand()
+        gen_player_hand(player_hand)
 
-    #print hands
-    print(f"Your hand is is {player_hand[0]} and {player_hand[1]}. Your opponent's hand is {comp_hand[0]} and {comp_hand[1]}")
-    player_keep = int(input("Which hand would you like to keep, 1 or 2? "))
-    player_final = player_hand[player_keep]
-    comp_final = comp_keep(comp_hand)
+        # print hands
+        print(f"Your hand is is {player_hand[0]} and {player_hand[1]}. Your opponent's hand is {comp_hand[0]} and {comp_hand[1]}")
+        player_keep = int(input("Which hand would you like to keep, 1 or 2? ")) - 1
+        player_final = player_hand[player_keep]
+        comp_final = comp_keep(comp_hand)
+
+        # evaluate winner
+        print(f"You chose {player_final} and your opponent chose {comp_final}. ")
+        print(winner_evaluation(comp_final, player_final, comp_score, player_score))
+        if(winner_evaluation(comp_final, player_final, comp_score, player_score) == "WIN"):
+            player_score += 1
+        if(winner_evaluation(comp_final, player_final, comp_score, player_score) == "LOSE"):
+            comp_score += 1
+        print(f"Current score is player: {player_score} Computer: {comp_score}")
+        keep_playing = input("Do you wish to keep playing? (True or False)") == "True"
+    print(f"Final score is player: {player_score} Computer: {comp_score}")
 
 if __name__ == "__main__":
     main()
