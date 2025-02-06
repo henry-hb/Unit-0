da872da5f53e2a403223f940d551391de2814987
henry-hb <hall-brh@wlhs.wlwv.k12.or.us>
2025-02-06 09:53:46 -0800

    initial commit

---
+++ Lesson-2/2D_Check.py
@@ -0,0 +1,32 @@
+"""
+pseudocode:
+Initialize the table data
+Print the table
+Compute and print the column totals
+"""
+
+
+def main():
+    populations = [
+        [106,107,111,133,221.767,1766],
+        [502,635,809,947,1402,3634,5268],
+        [2,2,2,6,13,30,46],
+        [163,203,26,408,547,729,628],
+        [2,7,26,82,172,307,392],
+        [16,24,38,74,167,511,809]
+    continents = [
+        "Africa",
+        "Asia",
+        "Australia",
+        "Europe",
+        "North America",
+        "South America"
+    ]
+
+
+
+
+    ]
+
+if __name__ == "__main__":
+    main()
