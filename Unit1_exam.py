87c3e898d4ee2bc4eb7384854d07d9dec2efd9c9
henry-hb <hall-brh@wlhs.wlwv.k12.or.us>
2025-02-25 14:28:49 -0800

    Update Unit1_exam.py

--- Unit1_exam.py
+++ Unit1_exam.py
@@ -11,8 +11,21 @@ def check_second(code):
     return "Urban"
 #checks if 1,3,5 are letters and 2,4,6 are numbers
 def check_validity(code):
-    if():
-        pass
+    odds = []
+    odds.append(str(code[0:1]))
+    odds.append(str(code[2:3]))
+    odds.append(str(code[4:5]))
+    evens = []
+    evens.append(str(code[1:2]))
+    evens.append(str(code[3:4]))
+    evens.append(str(code[5:]))
+    for i in odds
+        if (i.isAlpha()):
+            for j in evens:
+                if(j.isDigit()):
+                    return True
+            return True
+    return False
 
 def main():
     first_char = {
@@ -41,6 +54,10 @@ def main():
     while(not len(postal_code.strip()) == 6):
         print("Postal code is not the correct length to be valid in Canada. It should be 6 characters. Try again")
         postal_code=input("What is your postal code? ")
+    #checks to see if the characters are correct in the right indices
+    while(not check_validity(postal_code)):
+        print("Postal code is not the correct characters to be valid in Canada. \nThe 1st 3rd and 5th characters should be letters, and the 2nd 4th and 6th characters should be numbers. Try again")
+        postal_code=input("What is your postal code? ")
     #catches to see if first character is valid
     while(not check_first(postal_code, first_char)):
         print("Not a valid first character. Try again")
@@ -52,8 +69,7 @@ def main():
     #sets province and development to the characteristic indicated by code
     province = first_char[postal_code[0:1]]
     development = check_second(postal_code)
-    print(province)
-    print(development)
+    print(f"Your postal code is {postal_code}, and you live in {development} {province}.")
 
 
 
