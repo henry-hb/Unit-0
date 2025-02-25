3f3a4cde70249bb28cb55f853b6194caf5d58287
henry-hb <hall-brh@wlhs.wlwv.k12.or.us>
2025-02-25 09:55:25 -0800

    initial commit

--- Unit1_exam.py
+++ Unit1_exam.py
@@ -1,8 +1,18 @@
+#checks if first letter of postal code equals any keys in provinces
 def check_first(code, letters):
     for i in letters:
-        if(code[0] == letters[i]):
+        if(code[0:1].upper() == i):
             return True
     return False
+#checks if the second digit of postal code equals 0
+def check_second(code):
+    if(code[1:2] == "0"):
+        return "Rural"
+    return "Urban"
+#checks if 1,3,5 are letters and 2,4,6 are numbers
+def check_validity(code):
+    if():
+        pass
 
 def main():
     first_char = {
@@ -22,14 +32,30 @@ def main():
         "S" : "Saskatchewan",
         "T" : "Alberta",
         "V" : "British Columbia",
-        "X" : "Nunavut or NOrthwest Territories",
+        "X" : "Nunavut or Northwest Territories",
         "Y" : "Yukon"
     }
 
-    postal_code= input("What is your postal code")
-    while(check_first(postal_code, first_char) != True):
+    #catches to see if code is correct length
+    postal_code=input("What is your postal code? ")
+    while(not len(postal_code.strip()) == 6):
+        print("Postal code is not the correct length to be valid in Canada. It should be 6 characters. Try again")
+        postal_code=input("What is your postal code? ")
+    #catches to see if first character is valid
+    while(not check_first(postal_code, first_char)):
         print("Not a valid first character. Try again")
-        postal_code= input("What is your postal code")
+        postal_code=input("What is your postal code? ")
+    #catches if second digit is valid
+    while(not postal_code[1:2].isdigit()):
+        print("Not a valid second digit. It should be a number. Try again")
+        postal_code=input("What is your postal code? ")
+    #sets province and development to the characteristic indicated by code
+    province = first_char[postal_code[0:1]]
+    development = check_second(postal_code)
+    print(province)
+    print(development)
 
-if __name__ == "__main()__":
+
+
+if __name__ == "__main__":
     main()
