fe48d34ab64f20c8733ef6d6dfee0718eab54735
henry-hb <hall-brh@wlhs.wlwv.k12.or.us>
2025-02-27 17:18:41 +0000

    final commit

--- Unit1_exam.py
+++ Unit1_exam.py
@@ -9,19 +9,19 @@ def check_second(code):
     if(code[1:2] == "0"):
         return "Rural"
     return "Urban"
-#checks if 1,3,5 are letters and 2,4,6 are numbers
+#checks if 1,3,6 are letters and 2,5,7 are numbers and if 4 is a space
 def check_validity(code):
-    odds = []
-    odds.append(str(code[0:1]))
-    odds.append(str(code[2:3]))
-    odds.append(str(code[4:5]))
-    odd_string = "".join(odds)
-    evens = []
-    evens.append(str(code[1:2]))
-    evens.append(str(code[3:4]))
-    evens.append(str(code[5:]))
-    even_string = "".join(evens)
-    if (odd_string.isalpha() and even_string.isdigit):
+    lets = []
+    lets.append(str(code[0:1]))
+    lets.append(str(code[2:3]))
+    lets.append(str(code[5:6]))
+    letter_string = "".join(lets)
+    nums = []
+    nums.append(str(code[1:2]))
+    nums.append(str(code[4:5]))
+    nums.append(str(code[6:]))
+    num_string = "".join(nums)
+    if (letter_string.isalpha() and num_string.isdigit() and code[3:4] == " "):
         return True
     return False
 
@@ -49,12 +49,12 @@ def main():
 
     #catches to see if code is correct length
     postal_code=str(input("What is your postal code? "))
-    while(not len(postal_code.strip()) == 6):
+    while(not len("".join(postal_code.split())) == 6):
         print("Postal code is not the correct length to be valid in Canada. It should be 6 characters. Try again")
         postal_code=str(input("What is your postal code? "))
     #checks to see if the characters are correct in the right indices
     while(not check_validity(postal_code)):
-        print("Postal code is not the correct characters to be valid in Canada. \nThe 1st 3rd and 5th characters should be letters, and the 2nd 4th and 6th characters should be numbers. Try again")
+        print("Postal code is not the correct characters to be valid in Canada. \nThe 1st 3rd and 5th characters should be letters, and the 2nd 4th and 6th characters should be numbers.\nThere should be a space in between the two groups of three characters. Try again")
         postal_code=str(input("What is your postal code? "))
     #catches to see if first character is valid
     while(not check_first(postal_code, first_char)):
